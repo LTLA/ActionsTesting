ebc4fbe73b357689d842af8d5ce5074fdd3b580a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-19 13:50:09 -0700

    Hardened entrypoint to missing branch.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ${GITHUB_WORKSPACE}
-git checkout compiled
+git checkout -B  compiled
 git merge master
 
-git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git compiled

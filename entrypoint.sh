b5b09cd45494039f662aff95f0e63fecf8639c01
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-19 14:17:33 -0700

    Re-hardened to possibility of absent branch.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd ${GITHUB_WORKSPACE}
 git fetch --all
-git checkout compiled
+git checkout compiled 2>/dev/null || git checkout -b compiled 
 git merge master
 
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git

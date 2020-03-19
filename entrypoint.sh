a89b82add4a3887c2ddcff2b97b186b2e51bf437
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-19 14:00:51 -0700

    Just assume that someone has already made the 'compiled' branch.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd ${GITHUB_WORKSPACE}
 git fetch --all
-git checkout -B compiled
+git checkout compiled
 git merge master
 
-git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git compiled
+git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git

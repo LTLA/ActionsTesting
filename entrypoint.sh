371756fcdfdfcf485a59c06706452c31356ebc64
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-19 13:40:09 -0700

    Simplified workflow for example testing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/iSEE/screenshots
-git config --global user.email "infinite.monkeys.with.keyboards@gmail.com"
-git config --global user.name "LTLA"
-
-cd screenshots
+cd ${GITHUB_WORKSPACE}
 git checkout compiled
 git merge master
-
-git rm -rf images
-
-# Compiling the images.
-#CMD="Rdevel --slave --no-save"
-CMD=Rscript
-$CMD -e "source('compile.R')"
-
-# Committing everything in sight.
-git add images
-git commit -m "Recompiled PNGs."
 
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
